8bf05559f3c3910c6b8a416a57d164489f884a66
LEIVC <33700414+LEIVC@users.noreply.github.com>
2018-05-07 16:33:58 -0600

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,59 @@
+var friends = [ {
+    "name" : "John",
+    "photo" : "https://i.pinimg.com/originals/7f/ae/a0/7faea09dfd94717cc25350f72f8b9546.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Sheryl",
+    "photo" : "https://i.pinimg.com/originals/2e/12/49/2e12492e04a73b7d5884f9ca31eee744.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Kris",
+    "photo" : "https://i1.wp.com/gentlehair.com/wp-content/uploads/2015/08/mens-haircut-oval-face2.png?ssl=1",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+    "name" : "Alice",
+    "photo" : "https://www.sophiegee.com/wp-content/uploads/short-hairstyles-for-women-womens-haircuts-62411.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Garrett",
+    "photo" : "http://hairstylesweekly.com/images/2016/12/35-best-hairstyles-for-men-2017-popular-haircuts-for-guys-.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Kayla",
+    "photo" : "http://hairstylesweekly.com/images/2016/10/22-pretty-short-haircuts-for-women-.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Peter",
+    "photo" : "http://womenstyler.com/wp-content/uploads/2017/11/2017-mens-haircut.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Janice",
+    "photo" : "http://www.mediumhaircut99.com/wp-content/uploads/2016/10/medium-long-haircut-styles-medium-to-long-hairstyles-women-hairstyle-trendy.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Antwon",
+    "photo" : "http://nextluxury.com/wp-content/uploads/mens-haircut-part.jpg",
+    "scores" : [1,5,1,3,5,1,4,3,2,1]
+}, {
+    "name" : "Kristine",
+    "photo" : "https://www.short-haircut.com/wp-content/uploads/2017/12/Bob-Hairstyle-for-Women-396x300.jpg",
+    "scores" : [1,3,3,1,2,4,4,1,3,1]
+}, {
+    "name" : "Zayne",
+    "photo" : "http://nextluxury.com/wp-content/uploads/short-mens-classic-haircut.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Dani",
+    "photo" : "https://www.short-haircut.com/wp-content/uploads/2016/12/Super-Short-Womens-Haircuts.jpg",
+    "scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+    "name" : "Charlie",
+    "photo" : "https://www.shelookbook.com/wp-content/uploads/2015/07/Long-Hairstyles-for-Women-Over-50-no-Wrinkles.jpg",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}, {
+    "name" : "Oden",
+    "photo" : "https://barbarianstyle.net/wp-content/uploads/2016/02/Shoulder-length-side-part-haircut-for-men.jpg",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}];
+
+module.exports = friends;
